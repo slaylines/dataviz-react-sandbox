7aa4625254c4e6d10709c1e9a5e42461578f62b3
Nataliya <gnykka@gmail.com>
2024-07-24 20:43:16 +0400

    Comment the code and change sample data

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { Sparkline } from './components';
 
 const App: React.FC = () => {
-  const sparklineData = [
-    10, 12, 15, 9, 7, 8, 13, 16, 12, 10,
-    14, 18, 21, 19, 22, 24, 23, 20, 17,
-  ];
+  const sparklineData = Array.from({ length: 100 }, (_, index) => {
+    const x = (index / 99) * 4 * Math.PI;
+    return 4 * Math.sin(x);
+  });
+
 
   return (
     <div className="px-4 py-2">
       <h1 className="mb-2">Examples</h1>
       <h2 className="mb-2">1. Sparkline</h2>
-      <div className="w-48 h-16 p-1 border border-outline text-primary">
+      <div className="w-64 h-24 p-1 border border-outline text-primary">
         <Sparkline data={sparklineData} />
        </div>
     </div>
--- src/components/Sparkline.tsx
+++ src/components/Sparkline.tsx
@@ -5,11 +5,16 @@ type SparklineProps = {
 }
 
 const Sparkline: React.FC<SparklineProps> = ({ data }) => {
+  // Reference to the container element that is changeable
   const containerRef = useRef<HTMLDivElement>(null);
+  // Current dimensions for the chart
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
 
+  // Create effect to run when component mounts
   useEffect(() => {
     const container = containerRef.current;
+
+    // Do nothing if there is no element
     if (!container) return;
 
     const updateDimensions = () => {
@@ -19,22 +24,29 @@ const Sparkline: React.FC<SparklineProps> = ({ data }) => {
       });
     };
 
+    // Set initial dimensions
     updateDimensions();
 
+    // Create the observer and subscribe to container's changes
     const resizeObserver = new ResizeObserver(updateDimensions);
     resizeObserver.observe(container);
 
     return () => {
+      // Disconnect when the component unmounts
       resizeObserver.disconnect();
     };
   }, []);
 
+  // Don't render the chart for less than 2 points
   if (!data || data.length < 2) return null;
 
-  const { width, height } = dimensions;
+  // Calculate the min and max values of the data
   const min = Math.min(...data);
   const max = Math.max(...data);
 
+  // Construct the points string with the simple mapping
+  // Scales are [0, length] -> [0, width] and [min, max] -> [0, height]
+  const { width, height } = dimensions;
   const points = data.map((value, index) => {
     const x = (index / (data.length - 1)) * width;
     const y = ((value - min) / (max - min)) * height;
